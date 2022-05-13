f9113891fb202b4ed736537fa4b39f1ae2619618
kqzh <35095889+kqzh@users.noreply.github.com>
2022-05-13 15:31:05 +0800

    simplify log output

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,6 +1,10 @@
 package log
 
 import (
+	"path"
+	"runtime"
+	"strings"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -8,5 +12,14 @@ func init() {
 	logrus.SetReportCaller(true)
 	logrus.SetFormatter(&logrus.JSONFormatter{
 		TimestampFormat: "2006-01-02T15:04:05.000Z",
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			s := strings.Split(f.Function, ".")
+			funcname := s[len(s)-1]
+			_, filename := path.Split(f.File)
+			return funcname, filename
+		},
 	})
+
+	// Log output:
+	// {"file":"example.go","func":"example","level":"info","msg":"","time":"2006-01-02T15:04:05.000Z"}
 }
