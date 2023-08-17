2b3da3f42b35ccd95e304ff31d60b1f138ae4346
kqzh <35095889+kqzh@users.noreply.github.com>
2023-08-17 18:15:56 +0800

    feat: add get space usage logic

--- internal/clients/spaceusage.go
+++ internal/clients/spaceusage.go
@@ -1,6 +1,11 @@
 package clients
 
-import pb "github.com/vesoft-inc/nebula-agent/v3/pkg/proto"
+import (
+	pb "github.com/vesoft-inc/nebula-agent/v3/pkg/proto"
+	"os"
+	"path/filepath"
+	"strconv"
+)
 
 type SpaceUsage struct {
 	DataPath string
@@ -13,5 +18,28 @@ func NewSpaceUsage(dataPath string) *SpaceUsage {
 }
 
 func (s *SpaceUsage) GetSpaceUsages() (*pb.GetSpaceUsagesResponse, error) {
-	return nil, nil
+	spaceUsages := make([]*pb.GetSpaceUsagesResponse_SpaceUsageItem, 0)
+	err := filepath.Walk(s.DataPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			return nil
+		}
+		id, err := strconv.Atoi(info.Name())
+		if err != nil {
+			return nil
+		}
+
+		spaceUsages = append(spaceUsages, &pb.GetSpaceUsagesResponse_SpaceUsageItem{
+			Id:    int64(id),
+			Usage: info.Size(),
+		})
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetSpaceUsagesResponse{SpaceUsages: spaceUsages}, nil
 }
